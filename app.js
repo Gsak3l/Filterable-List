1b8bd61e78e254f20d6fe45fa07ac580a21bf0e4
gsak3l <gsakel5@gmail.com>
2019-12-21 10:46:03 +0200

    event listener added and function called on keyup

---
+++ app.js
@@ -0,0 +1,8 @@
+//get input element
+let filterInput = document.getElementById('filterInput');
+//add event listener to the element above
+filterInput.addEventListener('keyup', filterNames);
+
+function filterNames() {
+    console.log('hgodf');
+}
