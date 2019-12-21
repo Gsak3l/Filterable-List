772e8fe908f9863212f9ee57ad0bf4d4ee0f2104
gsak3l <gsakel5@gmail.com>
2019-12-21 11:09:23 +0200

    it works, more names added

--- app.js
+++ app.js
@@ -4,5 +4,20 @@ let filterInput = document.getElementById('filterInput');
 filterInput.addEventListener('keyup', filterNames);
 
 function filterNames() {
-    console.log('hgodf');
+    //getting the value of the input
+    let filterValue = document.getElementById('filterInput').value.toUpperCase();
+    //get names ul
+    let ul = document.getElementById('names');
+    //get li's from ul
+    let li = ul.querySelectorAll('li.collection-item');
+    //loop through collection-items li's
+    for (var i = 0; i < li.length; i++) {
+        let a = li[i].getElementsByTagName('a')[0];
+        //if it matches
+        if (a.innerHTML.toUpperCase().indexOf(filterValue) > -1) {
+            li[i].style.display = '';
+        } else {
+            li[i].style.display = 'none';
+        }
+    }
 }
